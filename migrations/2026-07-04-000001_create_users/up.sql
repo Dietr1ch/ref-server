93b55831c2890546906290a8c800affdd9e4fe0c
Dietrich Daroch <Dietrich@Daroch.me>
2026-07-06 18:37:36 -0400

    Docs: Add DB docs

--- migrations/2026-07-04-000001_create_users/up.sql
+++ migrations/2026-07-04-000001_create_users/up.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Users :/docs/db/users.org
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
