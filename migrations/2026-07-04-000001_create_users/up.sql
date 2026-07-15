ca999eda55c950d81e7fba955574e2c673742242
Dietrich Daroch <Dietrich@Daroch.me>
2026-07-15 16:02:41 -0400

    DB: Switch to UUIDv7

--- migrations/2026-07-04-000001_create_users/up.sql
+++ migrations/2026-07-04-000001_create_users/up.sql
@@ -1,8 +1,6 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 -- Users :/docs/db/users.org
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
--- migrations/2026-07-14-000002_create_posts/up.sql
+++ migrations/2026-07-14-000002_create_posts/up.sql
@@ -1,6 +1,6 @@
 -- Posts :/docs/db/posts.org
 CREATE TABLE posts (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     title VARCHAR(255) NOT NULL,
     body TEXT NOT NULL DEFAULT '',
